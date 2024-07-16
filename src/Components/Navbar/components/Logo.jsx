4e740f9d8c2a9ec953b2a7fba33e20020ee2d0a9
saramohamed124 <saramohamaden1234@gmail.com>
2024-07-16 03:53:00 +0300

    logoedit

--- src/Components/Navbar/components/Logo.jsx
+++ src/Components/Navbar/components/Logo.jsx
@@ -4,10 +4,10 @@ import logo_green from '../assets/img/logo_green.png'
 export function Logo() {
   return (
     <div className='flex-between-center gap-4'>
-      <Link to={'/'} className='text-[--main-color-green] font-bold text-3xl'>تراك</Link>
-      <Link to={'/'}>
+       <Link to={'/'}>
       <img className='max-w-[5rem]' src={logo_green} alt='logo-green'/>
       </Link>
+      <Link to={'/'} className='text-[--main-color-green] font-bold text-3xl'>تراك</Link>
     </div>
   );
 };

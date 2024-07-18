0b4b8b3a787d4ba25dda6bcd7ec428468c89104e
saramohamed124 <saramohamaden1234@gmail.com>
2024-07-18 19:49:57 +0300

    mapspage

--- src/Components/Home/components/search/components/Tabs.jsx
+++ src/Components/Home/components/search/components/Tabs.jsx
@@ -5,15 +5,15 @@ import map_icon from '../assets/icons/map_icon.svg';
 function Tabs() {
   return (
     <>
-      <ul className="avenir-heavy text-black w-fit flex-between-center gap-4 mt-7 mb-4 mr-5">
-        <li className="bg-[--main-color-orange-dark] flex-between-center gap-3 p-3 rounded-3xl">
-          <Link to="/hospitals" className="flex-between-center gap-3 ">
+      <ul className="search-tabs">
+        <li className="bg-[--main-color-orange-dark] search-tab hospital-search">
+          <Link to="/hospitals" className="link-search-tab">
             بحث
             <img src={search_icon} alt="search_icon" />
           </Link>
         </li>
-        <li className="bg-[--main-color-orange] flex-between-center gap-3 p-3 rounded-3xl transition-all duration-700 hover:bg-[--main-color-orange-dark]">
-          <Link to="/maps" className="flex-between-center gap-3 ">
+        <li className="bg-[--main-color-orange] search-tab maps-search">
+          <Link to="/maps" className="link-search-tab">
             خرائط
             <img src={map_icon} alt="map_icon" />
           </Link>

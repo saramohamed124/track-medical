5b56513b93638a307e595bdfdb5199500baae18a
saramohamed124 <saramohamaden1234@gmail.com>
2024-07-24 20:39:24 +0300

    chatmessage

---
+++ src/context/chat/ChatContext.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({children}) => {
+    // Live Chat => We will Need Define Users And Hospitals To GET Messages
+    const [users,setUser] = useState()
+    const [message,setMessage] = useState([]);
+
+    useEffect(()=>{
+        const getMessages = async () =>{
+            try{
+                // GET Messages From its Route In API
+                const res = await axios.get('/api/messages');
+                setMessage(res.data)
+            }catch(error){
+                // Print Error (For Test Only) 
+                // Here We should Make Icon Or any Thing else 
+                console.log('error');
+            }
+        }
+    },[]);
+
+    // Handle Send Messages
+    const handleSendMessage = async() => {
+        // We Should Make Message Here of user and Hospital and type it To handle Message Correctly
+        try{
+            const msg = await axios.post('/api/message/send');
+            setMessage(prevMessage => [...prevMessage, msg.data])
+        }catch(error){
+            console.log(error)
+        }
+    }
+return(
+    <ChatContext.Provider value={{message,users}}>{children}</ChatContext.Provider>
+)
+}
+
+export{ChatContext,ChatProvider}
